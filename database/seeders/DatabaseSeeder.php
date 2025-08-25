c5b038902713e3151271222e4c2511966e398d54
Daniel Karanja <sirkaranja12@gmail.com>
2025-08-25 22:51:23 +0300

    email verification paused for dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $admin = Role::create(['name' => 'admin']);
+        $user = Role::create(['name' => 'user']);
+        $organizer = Role::create(['name' => 'organizer']);
+        $customer = Role::create(['name' => 'customer']);
+
+        permission::create(['name' => 'manage users'])->assignRole($admin, $organizer);
+        permission::create(['name' => 'manage events'])->assignRole($admin, $organizer);
+        
+        $admin-> givePermissionTo(['manage users', 'manage events']);
+        $organizer-> givePermissionTo(['manage events']);
+
+
 
         User::factory()->create([
             'name' => 'Test User',
